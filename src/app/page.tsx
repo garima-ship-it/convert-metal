b2ebf7c813cb7165920688591a97a38f363d2c9f
garima-ship-it <garima@kashware.com>
2026-05-06 13:03:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import Navbar from '@/components/shared/Navbar'
 import ConvertHeroSection from '@/components/home/ConvertHeroSection'
 import BestsellerSection from '@/components/home/BestsellerSection'
-import SecurityFeaturesSection from '@/components/home/SecurityFeaturesSection'
-import SecuritySection from '@/components/home/SecuritySection'
-import CrossSellSection from '@/components/home/CrossSellSection'
-import RewardsSection from '@/components/home/RewardsSection'
-import FAQSection from '@/components/home/FAQSection'
-import HomeFooter from '@/components/home/HomeFooter'
 import StickyCTA from '@/components/shared/StickyCTA'
 import { fetchCatalog } from '@/lib/data'
 
@@ -17,16 +11,10 @@ export default async function HomePage() {
   return (
     <>
       <Navbar buyHref="/collection" />
-      <main style={{ paddingBottom: 87 }}>
+      <main>
         <ConvertHeroSection />
         <BestsellerSection cards={cards} />
-        <SecurityFeaturesSection />
-        <SecuritySection />
-        <CrossSellSection />
-        <RewardsSection />
-        <FAQSection />
       </main>
-      <HomeFooter />
       <StickyCTA />
     </>
   )
