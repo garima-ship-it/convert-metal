6480b447e55095113abd18bf71e723675279b529
garima-ship-it <garima@kashware.com>
2026-05-06 13:52:44 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
 import Navbar from '@/components/shared/Navbar'
 import ConvertHeroSection from '@/components/home/ConvertHeroSection'
+import BestsellerSection from '@/components/home/BestsellerSection'
+import SecurityFeaturesSection from '@/components/home/SecurityFeaturesSection'
+import FAQSection from '@/components/home/FAQSection'
+import HomeFooter from '@/components/home/HomeFooter'
+import StickyCTA from '@/components/shared/StickyCTA'
 import { fetchCatalog } from '@/lib/data'
 
 export default async function HomePage() {
   const catalog = await fetchCatalog()
+  const cards = catalog?.cards ?? []
 
   return (
     <>
       <Navbar buyHref="/collection" />
       <main>
         <ConvertHeroSection />
+        <BestsellerSection cards={cards} />
+        <SecurityFeaturesSection />
+        <FAQSection />
       </main>
+      <HomeFooter />
+      <StickyCTA />
     </>
   )
 }
