e9656a431b3d2d54078ef860d94603b074dee25d
garima-ship-it <garima@kashware.com>
2026-05-06 14:40:14 +0530

    Create RewardsSection.tsx

---
+++ src/components/home/RewardsSection.tsx
@@ -0,0 +1,12 @@
+export default function RewardsSection() {
+  return (
+    <section style={{ background: '#080808', width: '100%' }}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/images/home/rewardsBanner.png"
+        alt="Most Rewarding Payment Card"
+        style={{ width: '100%', display: 'block' }}
+      />
+    </section>
+  )
+}
