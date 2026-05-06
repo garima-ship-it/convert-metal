d317cd01f22acc93a100cd8c9f8922784aa3ba5e
garima-ship-it <garima@kashware.com>
2026-05-06 12:01:13 +0530

    Add files via upload

---
+++ src/app/convert-page.tsx
@@ -0,0 +1,33 @@
+import Navbar from '@/components/shared/Navbar'
+import ConvertHeroSection from '@/components/home/ConvertHeroSection'
+import BestsellerSection from '@/components/home/BestsellerSection'
+import SecurityFeaturesSection from '@/components/home/SecurityFeaturesSection'
+import SecuritySection from '@/components/home/SecuritySection'
+import CrossSellSection from '@/components/home/CrossSellSection'
+import RewardsSection from '@/components/home/RewardsSection'
+import FAQSection from '@/components/home/FAQSection'
+import HomeFooter from '@/components/home/HomeFooter'
+import StickyCTA from '@/components/shared/StickyCTA'
+import { fetchCatalog } from '@/lib/data'
+
+export default async function HomePage() {
+  const catalog = await fetchCatalog()
+  const cards = catalog?.cards ?? []
+
+  return (
+    <>
+      <Navbar buyHref="/collection" />
+      <main style={{ paddingBottom: 87 }}>
+        <ConvertHeroSection />
+        <BestsellerSection cards={cards} />
+        <SecurityFeaturesSection />
+        <SecuritySection />
+        <CrossSellSection />
+        <RewardsSection />
+        <FAQSection />
+      </main>
+      <HomeFooter />
+      <StickyCTA />
+    </>
+  )
+}
