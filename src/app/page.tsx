4991d64c7ca59fa2e5b6f6ee4cacd656a2092d5e
garima-ship-it <garima@kashware.com>
2026-05-06 14:26:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/shared/Navbar'
 import ConvertHeroSection from '@/components/home/ConvertHeroSection'
 import BestsellerSection from '@/components/home/BestsellerSection'
 import SecurityFeaturesSection from '@/components/home/SecurityFeaturesSection'
+import SecuritySection from '@/components/home/SecuritySection'
 import FAQSection from '@/components/home/FAQSection'
 import HomeFooter from '@/components/home/HomeFooter'
 import StickyCTA from '@/components/shared/StickyCTA'
@@ -18,6 +19,7 @@ export default async function HomePage() {
         <ConvertHeroSection />
         <BestsellerSection cards={cards} />
         <SecurityFeaturesSection />
+        <SecuritySection />
         <FAQSection />
       </main>
       <HomeFooter />
