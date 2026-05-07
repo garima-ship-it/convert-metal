31ea0224b64920c7b1efde9023b24a369142a881
garima-ship-it <garima@kashware.com>
2026-05-07 12:28:16 +0530

    Update RewardsSection.tsx

--- src/components/home/RewardsSection.tsx
+++ src/components/home/RewardsSection.tsx
@@ -5,8 +5,24 @@ export default function RewardsSection() {
       <img
         src="/images/home/rewardsBanner.png"
         alt="Most Rewarding Payment Card"
+        className="rewards-desktop"
         style={{ width: '100%', display: 'block' }}
       />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/images/home/rewardsBgMobile.png"
+        alt="Most Rewarding Payment Card"
+        className="rewards-mobile"
+        style={{ width: '100%', display: 'block' }}
+      />
+      <style>{`
+        @media (min-width: 769px) {
+          .rewards-mobile { display: none !important; }
+        }
+        @media (max-width: 768px) {
+          .rewards-desktop { display: none !important; }
+        }
+      `}</style>
     </section>
   )
 }
